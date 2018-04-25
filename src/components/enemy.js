f43b8d00306fee8052109b83c8a3eacf06fc1a67
Shang-Lin Chen <shanglin.chen@gmail.com>
2018-04-25 02:44:50 -0700

    Only remove the enemy if it wasn't hit by the ground.

--- src/components/enemy.js
+++ src/components/enemy.js
@@ -2,8 +2,10 @@ AFRAME.registerComponent('enemy', {
     init: function() {
         var el = this.el;
         el.addEventListener('collide', function(e) {
-            console.log("I'm hit by", e.detail.body.el.id);
-            el.parentNode.removeChild(el);
+            if (e.detail.body.el.id !== "ground") {
+                console.log("I'm hit by", e.detail.body.el.id);
+                el.parentNode.removeChild(el);
+            }
         });
     }
 
