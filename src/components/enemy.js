76e5bb0121f6459e0e83174aea1812931022f9a1
Shang-Lin Chen <shanglin.chen@gmail.com>
2018-04-25 02:31:02 -0700

    Try to print the ID of the other object in the collision.

--- src/components/enemy.js
+++ src/components/enemy.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('enemy', {
     init: function() {
         var el = this.el;
         el.addEventListener('collide', function(e) {
-            console.log("I'm hit!");
+            console.log("I'm hit by", e.detail.body.el.id);
             el.parentNode.removeChild(el);
         });
     }
